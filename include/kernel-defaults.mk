c7447c3c4a728cfbc180a44d576d1ffdf1186a18
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-05-19 22:49:28 +0300

    Mimic hash for 21.02 kernel to avoid warning on module install

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -106,7 +106,8 @@ define Kernel/Configure/Default
 		cp $(LINUX_DIR)/.config.set $(LINUX_DIR)/.config.prev; \
 	}
 	$(_SINGLE) [ -d $(LINUX_DIR)/user_headers ] || $(KERNEL_MAKE) INSTALL_HDR_PATH=$(LINUX_DIR)/user_headers headers_install
-	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | mkhash md5 > $(LINUX_DIR)/.vermagic
+	#grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | mkhash md5 > $(LINUX_DIR)/.vermagic
+	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | echo "c41339c4c098e0b7ad1ce585e773662d" > $(LINUX_DIR)/.vermagic
 endef
 
 define Kernel/Configure/Initramfs
