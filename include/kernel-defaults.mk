a0bd075269a421829ef2c0a16e40032d4cc17535
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-05-19 23:49:28 +0400

    Mimic hash for 23.05 kernel to avoid warning on module install

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -118,7 +118,8 @@ define Kernel/Configure/Default
 		cp $(LINUX_DIR)/.config.set $(LINUX_DIR)/.config.prev; \
 	}
 	$(_SINGLE) [ -d $(LINUX_DIR)/user_headers ] || $(KERNEL_MAKE) $(if $(findstring uml,$(BOARD)),ARCH=$(ARCH)) INSTALL_HDR_PATH=$(LINUX_DIR)/user_headers headers_install
-	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic
+	#grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic
+	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | echo "c2fa423ba3e559feec9cf347caa0e80e" > $(LINUX_DIR)/.vermagic
 endef
 
 define Kernel/Configure/Initramfs
