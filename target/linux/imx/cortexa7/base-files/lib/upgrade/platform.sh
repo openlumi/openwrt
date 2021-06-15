30f575ef3d1b58fb11ce975a75a80900526e4842
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-06-15 12:37:49 +0400

    imx: add Xiaomi Gateway DGNWG05LM support
    Xiaomi DGNWG05LM is a is a wall-mounted zigbee-to-internet gateway.
    
    Device specifications:
    * SoC: Freescale iMX6ULL
    * RAM: 256MiB
    * Flash: 256MiB NAND flash
    * Wireless 2.4GHz (RTL8723BS): b/g/n
    * Bluetooth 4.2 integrated in RTL8723BS
    * Zigbee Chip: NXP jn5169
    * Ring of RGB leds
    * BTN0 button
    * Sound chip TFA9882 + speaker
    
    Flashing instructions:
    
    Flashing requires a flashing tool from NXP: https://github.com/NXPmicro/mfgtools
    
    * solder wires and attach to system UART and usb connector
    * stop uboot by pressing any key
    * enter `bmode usb` to enable flashing over USB
    * run `mfgtoolcli` on the main computer to write kernel, dtb and rootfs to the device
    
    Known issues:
    - RTL8723BS driver is buggy and may cause freezes and kernel panics
    - Removing wdev disables the device until reboot. Patch is required.
    
    Signed-off-by: Ivan Belokobylskiy <belokobylskij@gmail.com>

--- target/linux/imx/cortexa7/base-files/lib/upgrade/platform.sh
+++ target/linux/imx/cortexa7/base-files/lib/upgrade/platform.sh
@@ -2,6 +2,15 @@ PART_NAME=firmware
 REQUIRE_IMAGE_METADATA=1
 
 platform_check_image() {
+	local board=$(board_name)
+
+	case "$board" in
+	xiaomi,dgnwg05lm )
+		local platform_dir_name=$(echo $board | sed 's/,/_/g')
+		nand_do_platform_check $platform_dir_name $1
+		return $?;
+		;;
+	esac
 	return 0
 }
 
@@ -9,7 +18,10 @@ platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
-	*)
+	xiaomi,dgnwg05lm )
+		nand_do_upgrade "$1"
+		;;
+  *)
 		default_do_upgrade "$1"
 		;;
 	esac
