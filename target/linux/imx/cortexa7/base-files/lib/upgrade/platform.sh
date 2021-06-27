e2d640ee3fa2471a3e184348fd245d6c08d88fb6
Ivan Belokobylskiy <belokobylskij@gmail.com>
2021-06-28 00:43:36 +0300

    imx: add Aqara Gateway ZHWG11LM support
    Aqara ZHWG11LM is a is a wall-mounted zigbee-to-internet gateway.
    It is almost a twin of Xiaomi DGNWG05LM with another wifi chip.
    
    Device specifications:
    * SoC: Freescale iMX6ULL
    * RAM: 256MiB
    * Flash: 256MiB NAND flash
    * Wireless 2.4GHz (RTL8189ES): b/g/n
    * Zigbee Chip: NXP jn5169
    * Ring of RGB leds
    * BTN0 button
    * Sound chip TFA9882 + speaker
    
    Flashing instructions:
    
    Flashing requires a flashing tool from NXP: https://github.com/NXPmicro/mfgtools
    
    * solder wires and attach to system UART and usb connector
    * stop uboot by pressing any key
    * enter `bmode usb` to enable flashing over USB
    * run `mfgtoolcli` on the main computer to write kernel, dtb and rootfs to the device
    
    Known issues:
    - RTL8189ES driver is buggy and may cause freezes and kernel panics
    - Removing wdev disables the device until reboot. Patch is required.
    
    Signed-off-by: Ivan Belokobylskiy <belokobylskij@gmail.com>

--- target/linux/imx/cortexa7/base-files/lib/upgrade/platform.sh
+++ target/linux/imx/cortexa7/base-files/lib/upgrade/platform.sh
@@ -5,6 +5,7 @@ platform_check_image() {
 	local board=$(board_name)
 
 	case "$board" in
+	aqara,zhwg11lm |\
 	xiaomi,dgnwg05lm )
 		local platform_dir_name=$(echo $board | sed 's/,/_/g')
 		nand_do_platform_check $platform_dir_name $1
@@ -18,6 +19,7 @@ platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
+	aqara,zhwg11lm |\
 	xiaomi,dgnwg05lm )
 		nand_do_upgrade "$1"
 		;;
